ad94443c80ebf27bbe7392e871ab97491d22ac83
Samartha Bapat <s4mbapat@gmail.com>
2026-05-12 12:35:48 -0500

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s: 
+            return False 
+        store = {'(': ')', '{': '}', '[':']'}
+        temp = [] 
+        for l in s : 
+            if l in store: 
+                temp.append(l)
+            else: 
+                if not temp: 
+                    return False
+                y = temp.pop() 
+                if store[y] ==l:
+                    continue 
+                else: 
+                    return False 
+        
+        return not temp
+'''
+()
+
+
+
+temp = [  (  { }
+
+
+'''        
