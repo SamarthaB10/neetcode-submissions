710462f4d4ca7d9b4bfd9627630cfd107b16bb10
Samartha Bapat <s4mbapat@gmail.com>
2026-06-06 21:56:19 -0500

    Add: sqrtx - submission-6

---
+++ Data Structures & Algorithms/sqrtx/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l,r = (0,x)
+        tg = 0 
+        while l<=r:
+            m = (l+r) // 2
+
+            if m*m <= x: 
+                tg = m
+                l = m+1
+            else:
+                r = m-1
+        return tg
+        
+
+'''
+l * l 
+
+
+
+'''
+
