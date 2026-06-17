dda2465d5b8a7779c97be8219e7b713b16371499
Samartha Bapat <s4mbapat@gmail.com>
2026-06-17 01:08:27 -0500

    Add: find-k-closest-elements - submission-1

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+
+        
+        vals = []
+        for v in arr:
+            dis = abs(v-x)
+            vals.append((dis,v))
+        
+       
+
+        vals = sorted(vals)
+        res = [] 
+        for i in range(k): 
+            res.append(vals[i][1])
+        return sorted(res)
+
+
+'''
+
+arr = [2,4,5,8]
+dict = {
+2:4
+4:2
+5:1
+8:3
+
+}
+
+'''
